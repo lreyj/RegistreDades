b4682e73e38795de9d5c5241db577a3dd2853f21
lreyj <lreyj@uoc.edu>
2012-03-28 18:05:59 +0200

    Versio .3: - Afegit 'About' - Afegit formulari amb controls de prova

--- app/view/PanelPrincipal.js
+++ app/view/PanelPrincipal.js
@@ -19,23 +19,22 @@ Ext.define('MyApp.view.PanelPrincipal', {
 
     config: {
         ui: 'light',
-        defaultBackButtonText: '\'Enrere\'',
+        defaultBackButtonText: 'Tornar',
         items: [
             {
                 xtype: 'nestedlist',
                 height: 300,
                 itemId: 'mynestedlist',
                 store: 'MenuList',
-                title: 'Opcions',
+                title: 'Registre cr&ograve;nics',
                 activeItem: 1,
                 toolbar: {
-                    xtype: 'toolbar',
-                    height: 40
+                    xtype: 'toolbar'
                 }
             }
         ],
         navigationBar: {
-            height: 40
+            height: 38
         },
         listeners: [
             {
@@ -55,13 +54,44 @@ Ext.define('MyApp.view.PanelPrincipal', {
                 styleHtmlContent:true,
                 items: [
                 {
-                    html: '<br>Registre de dades de pacients cr&ograve;nics<br>Versi&oacute; 1.0'
+                    html: [
+                    "<h2>Registre de dades per pacients cr&ograve;nics</h2><br>",
+                    "Registre de dades de pacients cr&ograve;nics<br>Versi&oacute; 1.0<br>",
+                    "<br><br> Autor: Lluis Rey",
+                    "<br>Coordinador: ..."
+                    ].join("")
                 }
                 ]
             });
-            this.push(tabPanel);
         }
-
+        else{
+            var tabPanel= Ext.create('Ext.Panel', {
+                title: 'Panel dinamic prova...',
+                items: [
+                {
+                    xtype: 'togglefield',
+                    label: 'Toggle'
+                },
+                {
+                    xtype: 'textfield',
+                    label: 'Texte'
+                },
+                {
+                    xtype: 'sliderfield',
+                    label: 'Slide'
+                },
+                {
+                    xtype: 'spinnerfield',
+                    label: 'Spinner1'
+                },
+                {
+                    xtype: 'spinnerfield',
+                    label: 'Spinner2'
+                }
+                ]
+            });
+        }
+        this.push(tabPanel);
     }
 
 });
